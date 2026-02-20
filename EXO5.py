dec288f837b921f6b8245c6e5275843ccede04c5
raphaeldebot <debot.raphael@gmail.com>
2026-02-20 15:34:18 +0100

    EXO5 improvment

--- EXO5.py
+++ EXO5.py
@@ -1,6 +1,6 @@
 a=input("Entrez un chiffre ou une lettre svp :")
 
-if ord(a) >= ord("0") and ord(a) <= ord("9"):
+if "0" <= a <= "9":
     print("c'est un chiffre")
 else:
     print("c'est pas un chiffre")
